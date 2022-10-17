ca4afd065fc3ef9c866d7ef254d3b87a7ad629f3
Ainoko Innocent <106755961+Princehinnex@users.noreply.github.com>
2022-10-17 02:56:34 +0100

    Done

--- get_precision.c
+++ get_precision.c
@@ -13,11 +13,13 @@ int precision = -1;
 if (format[curr_i] != '.')
 return (precision);
 precision = 0;
+  
 for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 {
 if (is_digit(format[curr_i]))
 {
 precision *= 10;
+  
 precision += format[curr_i] - '0';
 }
 else if (format[curr_i] == '*')
@@ -30,5 +32,6 @@ else
 break;
 }
 *i = curr_i - 1;
+ 
 return (precision);
 }
